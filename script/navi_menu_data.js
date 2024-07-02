e5165bf0a056b00f0a96b79f00ba4e308e4e2436
David.Chen <David.Chen@adaps-ph.com>
2024-07-02 10:44:13 +0800

    Reorganize the website categories and data.

--- script/navi_menu_data.js
+++ script/navi_menu_data.js
@@ -17,53 +17,192 @@ DNS服务器:
 */
 
 CoolSite=new Array(
-"日常必备",
+"日常使用",
 
 "https://metaso.cn","◆秘塔AI搜索","国内免费AI搜索",
 "https://kimi.moonshot.cn","◆Kimi.AI","很火的国内AI",
 "https://ai.xiabb.chat","◆阿水AI6.0","无需梯子chatGPT服务",
-"https://www.tiangong.cn","◆天工AI","据说不错",
-"https://gemini.google.com","★Google Gemini","Google的AI,效果一般",
 "https://www.google.com","★Google","AI前的王者,常需梯子", //ggssl.com
 "https://cn.bing.com/?ensearch=1","Bing国际版","无需梯子,无奈选择",
 "https://www.google.com/reader/","Google阅读器","",
 "www.google.com/ncr","Google/ncr","",   //no country redirect
-"www.ip-adress.com/ip_tracer/encrypted.google.com","★查加密谷歌IP","",
-"www.google.com/codesearch","Google代码搜索","",
-"code.google.com","Google Code","",
-"https://www.chaspark.com/#/patents/","◆查思专利","不注册也可查",
-"www.eet-china.com","电子工程专辑","",
-"www.icpcw.com","电脑报","",
+"www.cctv.com.cn/program/mzzlbg/06/index.shtml",'◆CCTV每周质量报告',"",
+"www.Zaobao.com",'联合早报网',"",
+"epaper.xkb.com.cn",'新快报',"",
+"www.nfcmag.com",'◆《南风窗》',"",
+"www.bochk.com",'中银香港',"",
 "","",""
 );
 
 Favorite1=new Array(
-"常去站点",
+"热门AI",
 
-"bbs.szhome.com","深圳房地产网论坛","",
-"blog.sina.com.cn/david1934",'◆阿源的Blog',"",
-"blog.sina.com.cn/u/1253425934",'◆阿源的Blog',"",
-"-","-----新闻杂志-----","",
-"touTiao.com",'头条网',"",
-"www.Zaobao.com",'联合早报网',"",
-"epaper.xkb.com.cn",'新快报',"",
-"www.sznews.com",'深圳新闻网',"",
-"www.cctv.com.cn/program/mzzlbg/06/index.shtml",'◆CCTV每周质量报告',"",
-"www.nfcmag.com",'◆《南风窗》',"",
-"-","-----美国价格参考-----","",
-"www.Amozon.com","Amozon","",
-"www.BestBuy.com","BestBuy","",
-"www.NewEgg.com","NewEgg","",
-"www.costco.com","costco会员店","",
-"www.samsclub.com","SAM's Club","",
-"www.consumersearch.com","ConsumerSearch","",
-"www.SkyMall.com","SkyMall","",
-"www.lowes.com","Lowe's","",
-"www.macys.com","Macy's","",
-"www.esteelauder.com","Estee Lauder","",
-"www.lordandtaylor.com","Lord & Taylor","",
-"www.waterfiltercomparisons.com","WaterFilterComparisons","",
-"www.waterfilters.net","WaterFilters.net","",
+"https://chatgpt.com","◆ChatGPT","最早最热的AI王者",
+"https://kimi.moonshot.cn","◆Kimi.AI","很火的免费国内AI",
+"https://www.tiangong.cn","◆天工AI","据说不错",
+"https://gemini.google.com","★Google Gemini","Google的AI,效果一般",
+"","",""
+);
+
+Favorite2=new Array(
+"在线字典.语言学习",
+
+"www.inputking.com","在线多语言输入","在线多语言输入",
+"www.cantoneseinput.com","◆在线粤语输入","◆在线粤语输入",
+"www.TypeIt.org","特殊语言符号输入","特殊语言符号输入",
+//“头发\发财”、“日历\经历”、“胡须\胡同” 必须 
+//“頭髮\發財”、“日曆\經歷”、“鬍鬚\衚衕” 必須
+"tool.webmasterhome.cn/GB2312_BIG5.asp","繁简文本转换","繁简文本转换",
+"www.wikipedia.org",'★WikiPedia',"维基百科",
+"www.differencebetween.info",'◆DiffBetween',"",
+"www.afterdawn.com/glossary/",'◆IT glossary',"",
+"www.zdic.net",'◆在线汉语字典',"",
+"www.edbchinese.hk/lexlist_ch/",'◆香港小學學習字詞表',"",
+"humanum.arts.cuhk.edu.hk/Lexis/lexi-can/",'◆粵語審音配詞字庫',"",
+
+"www.chineseetymology.org",'◆中文词源',"", // https://hanziyuan.net/
+
+"dict.bing.com.cn","◆微软bing词典","",
+"www.m-w.com","◆merriam-webster.com","",
+"oxforddictionaries.com","◆Oxford Dictionary","",
+"oald8.oxfordlearnersdictionaries.com","◆Oxford Advanced Dict.","",
+"www.ldoceonline.com","Longman OnlineDict","",
+"www.abbreviations.com","Abbreviations","",
+"dictionary.reference.com","Dictionary.com","",
+"dictionary.cambridge.org","CambridgeDict","",
+"www.dict.cn/search/","◆在线词典","",
+"cdict.net","线上英汉词典","",
+"https://dict.hjenglish.com/","沪江小D在线翻译","",
+"www.wordreference.com","免费多国语词典","",
+"dict.cnki.net","CNKI翻译助手","",
+"www.iciba.net","金山词霸在线词典","",
+"dict.yodao.com/","有道海量词典","",
+"translate.google.com/translate_t",'◆Google多语言翻译',"", //www.google.com/language_tools?hl=en
+"www.allwords.com",'西方多语言词典',"",
+"www.dict.org",'Dict.Org',"",
+"www.thefreedictionary.com",'Free Dictionary',"",
+"www.dictionarylink.com",'FreeOnline Dict.',"",
+"dictionary.cambridge.org","Cambridge Dict.","",
+"www.dictionary.com","Dictionary.com","",
+"www.onelook.com",'OneLook Dict.',"",
+"","",""
+);
+
+Favorite3=new Array(
+"实用工具",
+
+"Speedtest.net","◆Speedtest.net","网速测试",
+"www.linkwan.com/gb/broadmeter/","带宽测试1","网速测试",
+"iruler.net",'Online ruler','在线直尺',
+"www.online-stopwatch.com",'Online-Stopwatch','在线秒表',
+
+"www.docspal.com","文件格式转换","文件格式转换",
+"www.PdfUnlock.com","PDF Unlock","PDF文件解锁",
+"www.WebResizer.com","在线图片无损压缩","在线图片无损压缩",
+"compresspng.com","Compress PNG","PNG图片压缩",
+"https://tinypng.com/","Tiny PNG","PNG图片压缩",
+"vectormagic.com",'在线免费位图转矢量图','在线免费位图转矢量图',
+"barcode.tec-it.com",'在线免费生成条码','在线免费生成条码',
+
+"24timezones.com/map_zh.htm",'世界时钟','世界各地时间',
+"www.zdic.net/appendix/f27.htm",'◆择吉老黄历','◆择吉老黄历',
+"https://www.allconversions.com","Unit Conversion","单位换算(英文)", // https://www.lookuptables.com/
+"www.metric-conversions.org",'metric conversions',"单位换算(英文)",
+
+"https://play.google.com/store/apps",'<&Hopf;> Google Play应用商店',"最官方,但需登录且不能下载到电脑",
+"https://apps.evozi.com/apk-downloader/",'apps.evozi.com',"将Google Play应用下载到电脑",
+"https://apk-dl.com/",'apk-dl.com',"将Google Play应用下载到电脑",
+"https://www.apkmirror.com",'APKMirror',"将Google Play应用下载到电脑",
+"https://apkcombo.com",'APKCombo',"将Google Play应用下载到电脑",
+"https://en.aptoide.com/group/applications",'Aptoide',"将Google Play应用下载到电脑",
+"https://apkpure.com",'Apkpure',"将Google Play应用下载到电脑",
+"apkleecher.com",'apkleecher.com',"将Google Play应用下载到电脑",
+
+"www.virustotal.com",'◆单文件多引擎查毒1','上传文件在线查毒',
+"virusscan.jotti.org",'单文件多引擎查毒2','上传文件在线查毒',
+"www.virscan.org",'单文件多引擎查毒3','上传文件在线查毒',
+
+"www.virtualpiano.net",'◆在线钢琴','◆虚拟在线钢琴',
+"www.virtualmusicalinstruments.com",'◆虚拟乐器','◆虚拟各种乐器',
+"","",""
+);
+
+Favorite4=new Array(
+"外出旅行",
+
+"www.12306.cn",'◆12306.cn',"12306高铁火车票",
+"www.travelsky.com.cn",'◆信天游机票验真','机票验真',
+"www.ctrip.com",'携程旅行网',"机票酒店预定",
+"Qunar.com",'去哪儿机票搜索',"机票酒店预定",
+"www.shenzhenair.com",'深圳航空',"航空公司官网",
+"www.csair.com",'南方航空',"航空公司官网",
+
+"https://www.dialingcode.com/","dialing/country codes","国际电话区号查询",
+"lidicity.com/lieguo/huobi.html",'货币兑换换算器','货币兑换换算器',
+
+"https://ditu.amap.com/",'◆高德地图',"电脑查看地图效果更佳",
+"https://cn.bing.com/maps?FORM=Z9LH2",'◆必应地图',"电脑查看地图效果更佳",
+"www.openstreetmap.org",'◆OpenStreetMap',"电脑查看地图效果更佳",
+"www.tianditu.cn",'天地图',"电脑查看地图效果更佳",
+"www.earthol.com",'地球在线',"电脑查看地图效果更佳",
+"map.qq.com",'◆腾讯地图',"电脑查看地图效果更佳",
+
+"https://www.mtr.bj.cn",'北京京港地铁',"",
+"https://www.hncsmtr.com",'长沙轨道交通',"",
+"https://www.chengdurail.com",'成都轨道',"",
+"https://www.cqmetro.cn",'重庆轨道交通',"",
+"https://www.gzmtr.com",'广州地铁',"",
+"https://www.hzmetro.com",'杭州地铁',"",
+"https://www.njmetro.com.cn",'南京地铁',"",
+"www.nbmetro.com",'宁波轨道交通',"",
+"www.qd-metro.com",'青岛地铁',"",
+"www.shmetro.com",'上海地铁',"",
+"symtc.com",'沈阳地铁',"",
+"https://www.szmc.net",'深圳地铁',"",
+"https://www.sz-mtr.com",'苏州轨道交通',"",
+"https://www.wuhanrt.com",'武汉地铁',"",
+"https://www.xmgdjt.com.cn",'厦门地铁',"",
+"https://www.mtr.com.hk",'港鐵',"",
+"https://www.newmetro.taipei",'臺北捷運',"",
+"www.tjgdjt.com",'天津轨道交通',"",
+"https://www.zzmetro.cn",'郑州地铁',"",
+"","",""
+);
+
+Favorite5=new Array(
+"软件开发相关",
+
+"https://www.chaspark.com/#/patents/","◆查思专利","不注册也可查",
+"www.sipo.gov.cn/sipo/zljs/default.htm",'专利检索',"",
+"www.patent.com.cn",'中国专利信息网',"",
+
+"www.ip-adress.com/ip_tracer/encrypted.google.com","★查加密谷歌IP","",
+"www.google.com/codesearch","Google代码搜索","",
+"code.google.com","Google Code","",
+
+"www.all-nettools.com/toolbox","All Network Tools","", //216.92.207.177/tools1.htm
+"ipaddress.com","◆IP address tools","",
+"https://resolve.rs/ip/geolocation.html","◆IP Address Geolocation","",
+"tool.chinaz.com","◆站长工具","",
+"tool.webmasterhome.cn","◆站长工具2","",
+
+"https://www.asciitable.com/","ASCII table","",
+"https://www.lookuptables.com/text/ascii-table","ASCII table","",
+"https://www.lookuptables.com/text/html-character-codes",'HTML Character Codes',"",
+"https://home.unicode.org/",'unicode.org',"unicode标准官网",
+"https://www.lookuptables.com/text/unicode-characters","Unicode Characters and Blocks","",
+"https://symbl.cc/en/emoji/",'Full Emoji List',"",
+"","",""
+);
+
+Favorite6=new Array(
+"电脑菜鸟学苑",
+
+"https://www.runoob.com/","菜鸟教程","多种程序语言工具学习",
+"linux.vbird.org","◆鳥哥的Linux私房菜","历史悠久的Linux入门教程",
+"www.study-area.org",'◆台湾-酷！學園',"",
+"www.eet-china.com","电子工程专辑","",
+"www.icpcw.com","电脑报","",
 "","",""
 );
 
