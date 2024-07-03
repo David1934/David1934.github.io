c4080ca11f2b4d5731e52548476c0391c1094a2f
David.Chen <David.Chen@adaps-ph.com>
2024-07-03 16:17:20 +0800

    Add adaps office navigator

---
+++ script/navi_menu_data_adaps.js
@@ -0,0 +1,50 @@
+CoolSite=new Array(
+"日常使用",
+
+"support.adapsphotonics.com","Redmine","项目问题进度跟踪",
+"https://www.teambition.com","Teambition","问题跟踪,技术文档",
+"https://qiye.aliyun.com","阿里云企业邮箱","收发公司邮件",
+"https://ace.adapsphotonics.com:6002/login.xhtml","工时登记系统","查看填写工时,需在公司内网",
+"https://vm.adaps.cn","vmware云桌面","查看SVN资料,需在公司内网",
+"ci.adaps.corp/ReleaseServer","内部发布软件下载","需在公司内网",
+"gerrit.adaps.corp","软件gerrit服务器","需在公司内网",
+"ci.adaps.corp:8080","软件Jenkins服务器","需在公司内网",
+"","",""
+);
+
+Favorite1=new Array(
+"热门AI",
+
+"https://metaso.cn","◆秘塔AI搜索","国内免费AI搜索",
+"https://ai.xiabb.chat","◆阿水AI6.0","聚合AI,无需梯子chatGPT服务",
+"https://chat.aimoshu.cc","chatAI","聚合AI,无需梯子chatGPT服务",
+
+"https://chatglm.cn","◆智谱清言","质量不错",
+"https://kimi.moonshot.cn","◆Kimi.AI","很火的免费国内AI",
+"https://www.doubao.com/chat","◆豆包","字节跳动出品",
+"https://yuanbao.tencent.com/chat","◆腾讯元宝","腾讯出品",
+
+"https://tongyi.aliyun.com/qianwen","通义千问","阿里出品",
+"https://www.tiangong.cn","天工AI","国内AI",
+"https://xinghuo.xfyun.cn","讯飞星火","科大讯飞出品",
+"https://yiyan.baidu.com","文心一言","百度出品",
+"https://platform.baichuan-ai.com","百川","国内AI",
+
+"https://chat.openai.com","◆ChatGPT","最早最热的AI王者",
+"https://gemini.google.com","★Google Gemini","Google的AI,效果一般",
+"","",""
+);
+
+Favorite2=new Array(
+"软件开发相关",
+
+"https://www.chaspark.com/#/patents/","◆查思专利","不注册也可查",
+"www.sipo.gov.cn/sipo/zljs/default.htm",'专利检索',"",
+"www.patent.com.cn",'中国专利信息网',"",
+
+"https://www.runoob.com/","菜鸟教程","多种程序语言工具学习",
+"https://www.w3schools.com","Online web tutorials","英文版在线教程",
+"","",""
+);
+
+
