6c153c15d0dd5efefbac61bfd02eb0bdde14b44f
David.Chen <David.Chen@adaps-ph.com>
2024-07-07 14:37:00 +0800

    add navigator for high-school students.

---
+++ script/navi_menu_data_highschool.js
@@ -0,0 +1,120 @@
+CoolSite=new Array(
+"讲师导航",
+
+"https://space.bilibili.com/485578465","语文--国家玮-开明致学","",
+"https://space.bilibili.com/442129187","语文--乘风","",
+"","",""
+);
+
+Favorite1=new Array(
+"国家玮-合集·高考作文精讲",
+
+"https://www.bilibili.com/video/BV1Rk4y1p7yq","【全卷别】2023高考作文实况解析及命题趋势<br>24届提前抢分","",
+"https://www.bilibili.com/video/BV1St4y1H7d6","【22高考全国甲卷】30分钟详解<br>北大博士教你写出满分作文！","",
+"https://www.bilibili.com/video/BV1794y127GK","有手就行<br>北大博士带你写22新高考1卷作文","",
+"https://www.bilibili.com/video/BV1aB4y147kY","【22高考全国乙卷】考场作文满分选手<br>带你冲刺50+","",
+"https://www.bilibili.com/video/BV1EY411T7Yt","【22全国新高考Ⅱ卷】意识觉醒<br>突破写作瓶颈，带你上分","",
+"https://www.bilibili.com/video/BV19r4y137Jy","【22北京卷】大小作文思路讲解<br>作文50+真的不难","",
+"","",""
+);
+
+Favorite2=new Array(
+"国家玮-高考语文如何解锁130+",
+
+"https://www.bilibili.com/video/BV1zB4y1w7Sk","高考语文如何解锁130+<br>每个高中生都要get的底层逻辑","",
+"https://www.bilibili.com/video/BV1zB4y1w7Sk?p=2","高考语文如何解锁130+<br>每个高中生都要get的底层逻辑","",
+"https://www.bilibili.com/video/BV1zB4y1w7Sk?p=3","高考语文如何解锁130+<br>每个高中生都要get的底层逻辑","",
+"https://www.bilibili.com/video/BV1zB4y1w7Sk?p=4","高考语文如何解锁130+<br>每个高中生都要get的底层逻辑","",
+"https://www.bilibili.com/video/BV1zB4y1w7Sk?p=5","高考语文如何解锁130+<br>每个高中生都要get的底层逻辑","",
+"https://www.bilibili.com/video/BV1zB4y1w7Sk?p=6","高考语文如何解锁130+<br>每个高中生都要get的底层逻辑","",
+"https://www.bilibili.com/video/BV1zB4y1w7Sk?p=7","高考语文如何解锁130+<br>每个高中生都要get的底层逻辑","",
+"https://www.bilibili.com/video/BV1zB4y1w7Sk?p=8","高考语文如何解锁130+<br>每个高中生都要get的底层逻辑","",
+"","",""
+);
+
+Favorite3=new Array(
+"国家玮-语文",
+
+"https://www.bilibili.com/video/BV1QH4y1F7M5","暑期必做5件事！<br>作文提前抢跑，50+真的不难","",
+"https://www.bilibili.com/video/BV1sS411A7DG","高一高二必备答题术语<br>暑假学习效率翻倍，提分20+","",
+"","",""
+);
+
+Favorite4=new Array(
+"乘风-【文言文必备基础】",
+
+"https://www.bilibili.com/video/BV1HY4y137hT","310个高频文言文实词带背<br>part1  06:19","",
+"https://www.bilibili.com/video/BV1id4y1Q7Gk","310个高频文言文实词带背<br>part2  06:10","",
+"https://www.bilibili.com/video/BV1wB4y137Uk","310个高频文言文实词带背<br>part3  14:47","",
+"https://www.bilibili.com/video/BV1Z14y1V7Qz","310个高频文言文实词带背<br>part4  07:41","",
+"","",""
+);
+
+Favorite5=new Array(
+"乘风-【高考必背文化常识】",
+
+"https://www.bilibili.com/video/BV13i4y197Er","每天五分钟，轻松掌握！<br>称谓篇 05:59","",
+"https://www.bilibili.com/video/BV1Wa411z7u6","每天五分钟，轻松掌握！<br>官职篇1 04:10","",
+"https://www.bilibili.com/video/BV1eL411c7ED","每天五分钟，轻松掌握！<br>官职篇2 12:06","",
+"https://www.bilibili.com/video/BV1GL411w7jd","每天五分钟，轻松掌握！<br>天文立法1 09:35","",
+"https://www.bilibili.com/video/BV1YR4y1T7Ux","每天五分钟，轻松掌握！<br>天文立法2 05:40","",
+"https://www.bilibili.com/video/BV18b4y1776y","每天五分钟，轻松掌握！<br>古代地理1 07:09","",
+"https://www.bilibili.com/video/BV19U4y1f7Jx","每天五分钟，轻松掌握！<br>古代地理2 04:40","",
+"https://www.bilibili.com/video/BV12b4y1s7hc","每天五分钟，轻松掌握！<br>科举制度1 09:52","",
+"https://www.bilibili.com/video/BV1EP4y1g7oC","每天五分钟，轻松掌握！<br>科举制度2 07:08","",
+"https://www.bilibili.com/video/BV1Qu411i7Xx","每天五分钟，轻松掌握！<br>风俗礼节1 07:05","",
+"https://www.bilibili.com/video/BV1TY4y1p7M7","每天五分钟，轻松掌握！<br>风俗礼节2 08:01","",
+"https://www.bilibili.com/video/BV1Tu41167DL","每天五分钟，轻松掌握！<br>饮食器具/音乐文娱 07:16","",
+"","",""
+);
+
+Favorite6=new Array(
+"乘风-【高考语文一轮直击130+技巧】",
+
+"https://www.bilibili.com/video/BV1zx411f77t","一招破解近义成语辨析<br>part1 31:30","",
+"https://www.bilibili.com/video/BV1zx411f77t?p=2","速度基本功快练<br>part2","",
+"https://www.bilibili.com/video/BV1zx411f77t?p=3","part3","",
+"","",""
+);
+
+Favorite7=new Array(
+"乘风-【4节课冲击55+高分作文】",
+
+"https://www.bilibili.com/video/BV1W4411o7cn?","揭秘考场新套路！<br>作文上不了一类文原因揭秘","",
+"https://www.bilibili.com/video/BV1W4411o7cn?p=2","揭秘考场新套路！<br>审题如何不跑题偏题","",
+"https://www.bilibili.com/video/BV1zx411f77t?p=3","揭秘考场新套路！<br>作文素材自己编","",
+"https://www.bilibili.com/video/BV1zx411f77t?p=4","揭秘考场新套路！<br>作文刷题技巧","",
+"","",""
+);
+
+Favorite8=new Array(
+"乘风-语文",
+
+"https://www.bilibili.com/video/BV1b4411B7qS","学校从不讲的诗歌鉴赏技巧大全<br>30:37","",
+"","",""
+);
+
+Favorite10=new Array(
+"热门AI",
+
+"https://chat.openai.com","◆ChatGPT","最早最热的AI王者",
+"https://metaso.cn","◆秘塔AI搜索","国内免费AI搜索",
+"https://chat.aimoshu.cc","chatAI","聚合AI,无需梯子chatGPT服务",
+
+"https://chatglm.cn","◆智谱清言","质量不错",
+"https://kimi.moonshot.cn","◆Kimi.AI","很火的免费国内AI",
+"https://www.doubao.com/chat","◆豆包","字节跳动出品",
+"https://yuanbao.tencent.com/chat","◆腾讯元宝","腾讯出品",
+
+"https://tongyi.aliyun.com/qianwen","通义千问","阿里出品",
+"https://www.tiangong.cn","天工AI","国内AI",
+"https://xinghuo.xfyun.cn","讯飞星火","科大讯飞出品",
+"https://yiyan.baidu.com","文心一言","百度出品",
+"https://platform.baichuan-ai.com","百川","国内AI",
+
+"https://gemini.google.com","★Google Gemini","Google的AI,效果一般",
+"","",""
+);
+
+
+
