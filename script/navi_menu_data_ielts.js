e5ac415bbb6372e25cf4515a66d2a62e18fe404b
David.Chen <David.Chen@adaps-ph.com>
2025-05-05 10:50:59 +0800

    Add IELTS web page

---
+++ script/navi_menu_data_ielts.js
@@ -0,0 +1,56 @@
+CoolSite=new Array(
+"日常使用",
+
+"https://www.ielts.org","IELTS","IELTS offical website",
+"https://www.chinaielts.org","雅思中国官网","",
+"https://takeielts.britishcouncil.org","take ielts","英国文化协会",
+"https://ielts.idp.com","idp IELTS","",
+"https://takeielts.britishcouncil.org/take-ielts/prepare/free-ielts-english-practice-tests","free test","免费测试练习",
+"https://www.guixue.com/words/primary.shtml","雅思词汇真经-配套音频","及方法介绍视频",
+"https://ieltsonlinetests.com","IELTS online test","雅思在线测试",
+"https://ielts-simon.study/video-courses/","video-courses","",
+"https://www.ieltsbuddy.com","IELTS buddy","",
+"https://ieltsliz.com","IELTS Liz","前雅思考官",
+"https://ieltsliz.com/vocabulary","Liz vocabulary","Liz词汇",
+"https://ieltsmaterial.com","IELTS material","雅思素材",
+"https://www.bilibili.com/video/BV1c54y187SH","雅思真经第一课","雅思教父刘洪波",
+"https://www.bilibili.com/video/BV1rq4CeDE1h","雅思播客 全100集","油管最好的学雅思播客之一",
+"https://www.bilibili.com/video/BV1B4PpetEKf","一周吃透《雅思词汇真经》","",
+"https://www.bilibili.com/video/BV1tkcVe1EkZ","7天背完7000个雅思单词","《雅思词汇真经》硬核记忆",
+"https://www.bilibili.com/video/BV1aP1FYgEKx","漫画秒记3000英语单词","分场景记忆",
+"","",""
+);
+
+Favorite1=new Array(
+"搜索引擎 & 热门AI",
+
+"https://www.google.com","★Google","",
+"www.wikipedia.org",'★WikiPedia',"维基百科",
+"https://metaso.cn","秘塔AI搜索","国内免费AI搜索",
+"https://n.cn","纳米搜索","原360 AI搜索升级",
+"https://cn.bing.com/?ensearch=1","Bing国际版","无需梯子",
+"https://ai-bot.cn","AI工具集","AI工具分类导航",
+
+"https://ai.xiabb.chat","◆阿水AI6.0","聚合AI,无需梯子chatGPT服务",
+
+"https://github.com/copilot","◆Copilot","Github Copilot",
+"https://www.deepseek.com","◆DeepSeek","又一个免费国内AI",
+"https://www.doubao.com/chat","◆豆包","字节跳动出品",
+"https://kimi.moonshot.cn","◆Kimi.AI","很火的免费国内AI",
+
+"https://yiyan.baidu.com","文心一言","百度出品",
+"https://www.tiangong.cn","天工AI","国内AI",
+"https://chatglm.cn","智谱清言","质量不错",
+
+
+"https://tongyi.aliyun.com/qianwen","通义千问","阿里出品",
+"https://xinghuo.xfyun.cn","讯飞星火","科大讯飞出品",
+
+"https://yuanbao.tencent.com/chat","腾讯元宝","腾讯出品",
+
+
+"https://chat.openai.com","★◆ChatGPT","最早最热的AI王者",
+"https://gemini.google.com","★Google Gemini","Google的AI,效果一般",
+"","",""
+);
+
