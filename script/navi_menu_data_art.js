e3504a9556847c4e1e11ae2127d912a5e5ddadd4
David.Chen <David.Chen@adaps-ph.com>
2026-08-09 22:35:19 +0800

    Add art_nav.html

---
+++ script/navi_menu_data_art.js
@@ -0,0 +1,58 @@
+var Categories = [];
+
+Categories.push(new Array(
+"搜索引擎 & 热门AI",
+
+"www.google.com/ncr","★Google/ncr","",   //no country redirect
+"www.wikipedia.org",'★WikiPedia',"维基百科",
+"https://metaso.cn","秘塔AI搜索","国内免费AI搜索",
+"https://n.cn","纳米搜索","原360 AI搜索升级",
+"https://cn.bing.com/?ensearch=1","Bing国际版","无需梯子",
+"https://yandex.com","Yandex","无需梯子",
+"https://ai-bot.cn","AI工具集","AI工具分类导航",
+
+//"https://ai.xiabb.chat","◆阿水AI6.0","聚合AI,无需梯子chatGPT服务",
+"https://ish.chat","◆ish.chat","",
+
+"https://github.com/copilot","◆Copilot","Github Copilot",
+"https://www.deepseek.com","◆DeepSeek","又一个免费国内AI",
+"https://www.doubao.com/chat","◆豆包","字节跳动出品",
+"https://kimi.moonshot.cn","◆Kimi.AI","◆可以长文档输入",
+
+"https://www.codebuddy.cn","腾讯workbuddy","腾讯出品",
+
+"https://chat.openai.com","★◆ChatGPT","最早最热的AI王者",
+"https://gemini.google.com","★Google Gemini","Google的AI,效果一般",
+"","",""
+));
+
+Categories.push(new Array(
+"工具",
+"https://www.njmetro.com.cn","南京地铁","",
+"https://www.qijishow.com/down/index.html",'设计软件下载',"",
+"https://www.qijishow.com/down/Office%202024.html",'Office2024',"",
+"https://www.qijishow.com/down/standard.html",'屏幕尺寸大全',"",
+"https://www.qijishow.com/down/ios.html",'iOS/Android设计尺寸规范',"",
+"https://www.qijishow.com/Colors/gradient.html",'色彩空间',"",
+"https://www.qijishow.com/resource/fonts.html",'免费字体',"",
+"https://aixyk.com/",'AI小样库',"",
+"https://developer.apple.com/cn/design/human-interface-guidelines",'人机界面指南',"Apple苹果",
+"https://www.photopea.com",'photopea',"在线类ps免费软件",
+"https://krita.org/zh-cn/post-download",'Krita免费教学',"",
+
+"https://www.bilibili.com","BiliBili","",
+"https://www.runoob.com/python3/python3-tutorial.html","Python 3教程","菜鸟教程",
+
+"https://portableapps.com",'PortableApps',"",
+"https://www.drawio.com",'◆draw.io',"免费的画图工具",
+"https://www.markdownonline.net/",'◆在线MD编辑器',"Pro Markdown Online Editor",
+"https://dillinger.io",'◆dillinger.io',"开源在线MD编辑器",
+"https://pandao.github.io/editor.md/",'◆开源在线MD编辑器',"",
+"https://md.doocs.org/",'◆在线MD编辑器',"https://github.com/doocs/md",
+"https://html5-editor.net/",'◆在线H5编辑器',"html5 editor",
+
+"https://gzxzgwl.2017mac.cn/",'广州青学在线',"",
+
+"","",""
+));
+
